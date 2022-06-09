de4871f3d8035fa9c0505f1d10fd2a58fcac31c5
BelovDmitry <1@belov.es>
2022-06-09 21:46:26 +0300

    Пустой проект

---
+++ GBShop/GBShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBShopApp.swift
+//  GBShop
+//
+//  Created by Dmitry Belov on 09.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GBShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
